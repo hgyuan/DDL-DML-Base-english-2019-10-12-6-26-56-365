ad6cdfb22733977c5e974a044456aceb05afcf50
1764775976@qq.com <1764775976@qq.com>
2020-07-01 17:21:33 +0800

    update

--- dml.sql
+++ dml.sql
@@ -1,7 +1,10 @@
 -- Insert record
-
+insert into table_name (字段) values();
 -- Revise record
-
+UPDATE table_name
+    -> SET 字段=数据
+    -> WHERE 条件;
 --  Delete record
-
+DELETE FROM tb_courses_new;
 -- Search record
+select * from 表名 where 条件;
